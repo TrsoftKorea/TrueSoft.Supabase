04e7d9a1cde06ebce4c76459a513d46f7776279f
ljuh1521 <ljuh1521@naver.com>
2026-05-20 13:31:20 +0900

    Update PostgrestOpenApiUserSaveClass.cs

--- Editor/PostgrestOpenApiUserSaveClass.cs
+++ Editor/PostgrestOpenApiUserSaveClass.cs
@@ -222,7 +222,7 @@ namespace Truesoft.Supabase.Editor
             sb.AppendLine(indent + "    public static readonly " + className + " Instance = new();");
             sb.AppendLine(indent + "    private " + className + "() : base() { }");
             sb.AppendLine();
-            sb.AppendLine(indent + "    public static System.Threading.Tasks.Task<bool> TryLoadAsync() => Instance.TryLoadAsync();");
+            sb.AppendLine(indent + "    public static System.Threading.Tasks.Task<bool> TryLoadAsync() => ((StaticUserSave<Row>)Instance).TryLoadAsync();");
             sb.AppendLine();
             sb.AppendLine(indent + "    [Serializable]");
             sb.AppendLine(indent + "    public sealed class Row");
